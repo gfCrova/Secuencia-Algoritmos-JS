3a280e4f57582f148a6cdfa716720115864c97cf
gfCrova <giancrova.cl5@gmail.com>
2022-11-17 00:18:44 -0300

    funcion string Truncado

--- 1- Basicos/06- Secuencias-con-Strings.js
+++ 1- Basicos/06- Secuencias-con-Strings.js
@@ -1,4 +1,6 @@
+
 // Concatenar un String(str), Tantas veces(num)
+
 const repeatStringNumTimes = (str, num) => {
     let newStr = "";
     for (let i = 0; i < num; i++) {
@@ -9,10 +11,28 @@ const repeatStringNumTimes = (str, num) => {
 repeatStringNumTimes("Text", 3); // OUTPUT: TextTextText
 
 
+/*****************************************************************************************/
+
 
 // Verificar si el String(str) TERMINA con la cadena especificada (target) 
+
 const confirmEnding = (str, target) => {
     return str.slice(-target.length) === target  
 }
-
 console.log(confirmEnding("Bastian", "an")); // OUTPUT: True
+
+
+/*****************************************************************************************/
+
+
+//  Si el String(str) es más largo que la longitud máxima (num). Devuelve la cadena Truncada con...!
+
+const truncateString = (str, num) => {
+    if(str.length > num){
+        return str.substr(0, num) + "...";
+    } else {
+        return str;
+    }
+}
+
+console.log(truncateString("A-tisket a-tasket A green and yellow basket", 8)); // OUTPUT: A-tisket...
